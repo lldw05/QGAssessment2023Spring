9612c10a6778c5dc813b5d660e67513996ca9683
lldw <2053201883@qq.com>
2023-03-23 10:38:45 +0800

    小组作业 CRUD Utils完成

---
+++ MinGroup/jdbcUtils/src/Main.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ * @author LLDW
+ * @date 2023年3月22日17:37:13
+ */
+public class Main {
+    @Test
+    public void test(){
+        JdbcUtils ju = JdbcUtils.getInstance();
+        Object[] data = {};
+        ArrayList<Object> list = ju.execQueryList("select * from account",null);
+        int cnt = ju.update("delete from account where id = ?",1);
+        System.out.println(cnt);
+        System.out.println(list);
+    }
+
+}
